156607cd11fc633c85893feba8330dbbf041b066
fearofvoid <wardennkoil@gmail.com>
2025-01-29 15:51:33 -0500

    authorization wip

--- router/router.js
+++ router/router.js
@@ -1,14 +1,17 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import LoginPage from "../src/views/Authorization/LoginPage.vue";
+import SignupPage from "../src/views/Authorization/SignupPage.vue";
 
 
 
 const routes = [
     { path: '/login', component: LoginPage },
-    { path: '/signup', component: AboutView },
+    { path: '/signup', component: SignupPage },
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 })
+
+export default router;
--- src/views/Authorization/SignupPage.vue
+++ src/views/Authorization/SignupPage.vue
@@ -1,23 +1,15 @@
 <script>
 export default {
-  name: "LoginPage"
+  name: "SignupPage"
 }
 </script>
 
 <template>
-    <!--
-      This example requires updating your template:
-
-      ```
-      <html class="h-full bg-white">
-      <body class="h-full">
-      ```
-    -->
   <div class="h-full w-full flex items-center justify-center">
     <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-        <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+        <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Create a new account account</h2>
       </div>
 
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -32,24 +24,29 @@ export default {
           <div>
             <div class="flex items-center justify-between">
               <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
-<!--              <div class="text-sm">-->
-<!--                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>-->
-<!--              </div>-->
             </div>
             <div class="mt-2">
               <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
             </div>
           </div>
+          <div>
+            <div class="flex items-center justify-between">
+              <label for="confirm-password" class="block text-sm/6 font-medium text-gray-900">Confirm Password</label>
+            </div>
+            <div class="mt-2">
+              <input type="text" name="confirm-password" id="confirm-password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            </div>
+          </div>
 
           <div>
-            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign up</button>
           </div>
         </form>
 
         <p class="mt-10 text-center text-sm/6 text-gray-500">
-          Not a member?
+          Already a member?
           {{ ' ' }}
-          <router-link to="/signup" class="font-semibold text-indigo-600 hover:text-indigo-500">Sign Up!</router-link>
+          <router-link to="/login" class="font-semibold text-indigo-600 hover:text-indigo-500">Log In!</router-link>
         </p>
       </div>
     </div>
